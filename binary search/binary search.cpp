7663748051bbfe934ab9e19a9614b5ecb2ca0360
tix01 <102047819+tix01@users.noreply.github.com>
2022-08-28 20:36:50 +0300

    Add files via upload

---
+++ binary search/binary search.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<vector>
+int BinarySearch(int value, std::vector<int> vec) {
+	int l = 0;
+	int r = vec.size();
+	int i =(l+r)/2;
+	while (vec[i] != value) {
+		if (vec[i] > value) {
+			r = i-1;
+			i = ((l + r) / 2);
+		}
+		else if (vec[i] < value) {
+			l = i+1;
+			i = ((l + r) / 2);
+		}
+	
+	}
+	return vec[i];
+
+}
+int main(){
+
+	return 0;
+}
